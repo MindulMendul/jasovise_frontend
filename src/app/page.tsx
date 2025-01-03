5e18fb7a1fb1f450d742eaa7322cd2dd76b4b03e
Mindul <pencake33@naver.com>
2025-01-04 00:48:05 +0900

    zustand로 데이터 패칭할 수 있도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+
+import useJaso from "@/app/_store/store";
+import { ChangeEvent, useCallback } from "react";
+
 export default function Home() {
+  const { jaso, setJaso } = useJaso();
+
+  const handleJaso = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setJaso(e.target.value);
+    },
+    [jaso, setJaso]
+  );
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>자소바이저</h1>
---
+++ src/configs/axios.ts
@@ -0,0 +1,25 @@
+import {
+  onPrivateRequest,
+  onPrivateResponseError,
+  onPublicRequest,
+  onPublicResponseError,
+  onRequestError,
+  onResponse,
+} from "@/configs/interceptor";
+import axios from "axios";
+
+// 인증이 필요없는 인스턴스
+export const publicApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASEURL,
+  timeout: 10000,
+});
+publicApi.interceptors.request.use(onPublicRequest, onRequestError);
+publicApi.interceptors.response.use(onResponse, onPublicResponseError);
+
+// 서버 사이드에서 사용할 인스턴스
+export const privateApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASEURL,
+  timeout: 10000,
+});
+privateApi.interceptors.request.use(onPrivateRequest, onRequestError);
+privateApi.interceptors.response.use(onResponse, onPrivateResponseError);
